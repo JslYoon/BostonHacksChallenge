e3f916a5d163db1a6d5fa0905a713a33feb09768
Lucas <junsuny789@gmail.com>
2022-04-21 00:37:14 -0400

    sorry Marco

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import TodoList from './TodoList';
 
 function App() {
-  const [getMessage, setGetMessage] = useState({})
+  const [getMessage, setGetMessage] = useState([])
 
   useEffect(()=>{
-    axios.get('http://localhost:5000/hello').then(response => {
+      axios.get('http://localhost:5000/hello').then(response => response.json().then(data => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
       console.log(error)
-    })
-
+    }) 
+      );
   }, [])
 
   return (
-    <div className="App">
-      <div>{getMessage.status === 200 ? 
-        <h3>{getMessage.data.message}</h3>
-        :
-        <h3>LOADING</h3>}
-      </div>
-    </div>
-  );
+    <>
+   <TodoList todos={getMessage}/> 
+   <input type = "text"></input>
+   <button>Add Todo</button>
+   <button>Clear Completed Todos</button>
+   <div>hello world</div>
+   </>
+  )
 }
 
 export default App;
